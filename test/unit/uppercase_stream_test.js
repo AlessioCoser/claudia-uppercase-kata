84d8b58cda221488cc80c679e366f315d387fe88
alessiocoser <alessio.coser@xpeppers.com>
2016-10-13 14:07:30 +0200

    add unit test for uppercaseStream
    use uppercaseStream module

---
+++ test/unit/uppercase_stream_test.js
@@ -0,0 +1,24 @@
+var aws = require('aws-sdk');
+var assert = require('assert')
+
+var uppercaseStream = require("../../lib/uppercase_stream").uppercaseStream
+
+var Stream = require('stream');
+
+
+describe('UppercaseStream', function(){
+  it('converts stream to uppercase', function(done){
+
+    var stream = new Stream();
+
+    outputStream = uppercaseStream(stream);
+
+    outputStream.on('data', function(chunk) {
+      console.log(chunk.toString());
+      assert.equal(chunk.toString(), "QUESTA È UNA MIA STRINGA")
+      done()
+    })
+
+    stream.emit("data", "questa è una mia stringa")
+  })
+})
